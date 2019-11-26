45de2ebbc3938d3c10d769b9282ed79f409fa599
Moshe Boord <mcboord@gmail.com>
2019-11-26 12:13:12 -0500

    added less than and greater than

--- src/App.js
+++ src/App.js
@@ -1,75 +1,75 @@
 import React, { Component, Fragment } from 'react'
-import {connect} from 'react-redux'
-import {sendMessage} from './chat'
+import { connect } from 'react-redux'
+import { sendMessage } from './chat'
 import './App.css'
-import { 
-  Row, 
-  Jumbotron, 
+import {
+  Row,
+  Jumbotron,
   Toast,
   ToastBody,
   ToastHeader,
   Navbar,
   NavbarBrand,
-  } from 'reactstrap'
+} from 'reactstrap'
 
 export class App extends Component {
 
   render() {
-    const {feed, sendMessage} = this.props
+    const { feed, sendMessage } = this.props
 
     function bottom() {
-      document.getElementsByClassName( 'user-message-field' )[0].scrollIntoView();
+      document.getElementsByClassName('user-message-field')[0].scrollIntoView();
     };
 
     return (
       <div>
         <div>
           <Jumbotron className="header">
-            <h1>Welcome to Squeezy!</h1>         
+            <h1>Welcome to Squeezy!</h1>
           </Jumbotron>
           <Navbar color="dark">
-              <NavbarBrand href="/" className="Home">Home
+            <NavbarBrand href="/" className="Home">Home
               </NavbarBrand>
           </Navbar>
           <p className="tagline">SQL Made Easy For Your Convenience</p>
-          
+
         </div>
-      
+
         <div className="main">
           {feed.map((entry, idx) => {
             return (
               <div>
-              <Row key={idx} className="speech-bubble-right">
-                <div className='speech-bubble-text-body'>
-                  <p><strong>Demo Speech Bubble</strong></p>
-                  <p>{entry.text}</p>
-                  {entry.choices.length > 0 ?
-                    <ol>
-                      {entry.choices.map(choice => <li>{choice}</li>)}
-                    </ol>
-                    :
-                    null
-                  }
-                  <div class="speech-bubble-right-arrow"></div>
-                </div>
-              </Row>
-              <br />  
+                <Row key={idx} className="speech-bubble-right">
+                  <div className='speech-bubble-text-body'>
+                    <p><strong>Demo Speech Bubble</strong></p>
+                    <p>{entry.text}</p>
+                    {entry.choices.length > 0 ?
+                      <ol>
+                        {entry.choices.map(choice => <li>{choice}</li>)}
+                      </ol>
+                      :
+                      null
+                    }
+                    <div class="speech-bubble-right-arrow"></div>
+                  </div>
+                </Row>
+                <br />
               </div>
             )
-            }           
+          }
           )}
-        </div> 
-        <div>
-          
         </div>
-           <input className='user-message-field' type="text" placeholder= "Type Response Here" onKeyDown={(e) => {
-             if (e.keyCode === 13) {
-              document.getElementsByClassName( 'speech-bubble-right' )[0].scrollIntoView();
-               sendMessage(e.target.value)
-               e.target.value = ''
-             }
-           }}
-           />
+        <div>
+
+        </div>
+        <input className='user-message-field' type="text" placeholder="Type Response Here" onKeyDown={(e) => {
+          if (e.keyCode === 13) {
+            document.getElementsByClassName('speech-bubble-right')[0].scrollIntoView();
+            sendMessage(e.target.value)
+            e.target.value = ''
+          }
+        }}
+        />
       </div>
     )
   }
@@ -82,4 +82,4 @@ const mapStateToProps = state => ({
   feed: state
 })
 
-export default connect(mapStateToProps, {sendMessage})(App)
+export default connect(mapStateToProps, { sendMessage })(App)
