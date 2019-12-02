95b983d9eca1351192cf4c322e3854667287dfb7
droza0 <droza0@users.noreply.github.com>
2019-12-02 12:50:47 -0500

    added redux-localstorage to persist redux store on refresh

--- src/store.js
+++ src/store.js
@@ -1,19 +1,27 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import chat, {messageMiddleware, logger} from './chat'
 import data from './data'
+import persistState from 'redux-localstorage'
+
+const enhancer = compose(
+    persistState()
+)
 
 
 const reducer = combineReducers({
     chat,
     data,
 })
-const middleware = composeWithDevTools(
-    applyMiddleware(thunkMiddleware, messageMiddleware, logger)
+const enhancers = composeWithDevTools(
+    applyMiddleware(thunkMiddleware, messageMiddleware, logger),
+    enhancer
 )
-const store = createStore(reducer, middleware)
+
+const store = createStore(reducer, enhancers)
 
 export default store
+
 export * from './chat'
 export * from './data'
